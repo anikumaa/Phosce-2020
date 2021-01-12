9254dfec3a8453e93128938300572a65c338ad37
NavSous <navhs360@gmail.com>
2021-01-11 16:51:07 -0800

    Added the star selection

---
+++ star.js
@@ -0,0 +1,90 @@
+function deselect(){
+    let s_1 = document.getElementById("1");
+    let s_2 = document.getElementById("2");
+    let s_3 = document.getElementById("3");
+    let s_4 = document.getElementById("4");
+    let s_5 = document.getElementById("5");
+    s_1.setAttribute("style","color:black;")
+    s_2.setAttribute("style","color:black;")
+    s_3.setAttribute("style","color:black;")
+    s_4.setAttribute("style","color:black;")
+    s_5.setAttribute("style","color:black;")
+    let selected = 0;
+    let b = document.getElementById("value");
+    b.innerHTML = selected;
+}
+function one(){
+    let s_1 = document.getElementById("1");
+    let s_2 = document.getElementById("2");
+    let s_3 = document.getElementById("3");
+    let s_4 = document.getElementById("4");
+    let s_5 = document.getElementById("5");
+    s_1.setAttribute("style","color:#00cc66;")
+    s_2.setAttribute("style","color:black;")
+    s_3.setAttribute("style","color:black;")
+    s_4.setAttribute("style","color:black;")
+    s_5.setAttribute("style","color:black;")
+    let selected = 1;
+    let b = document.getElementById("value");
+    b.innerHTML = selected;
+}
+function two(){
+    let s_1 = document.getElementById("1");
+    let s_2 = document.getElementById("2");
+    let s_3 = document.getElementById("3");
+    let s_4 = document.getElementById("4");
+    let s_5 = document.getElementById("5");
+    s_1.setAttribute("style","color:#00cc66;")
+    s_2.setAttribute("style","color:#00cc66;")
+    s_3.setAttribute("style","color:black;")
+    s_4.setAttribute("style","color:black;")
+    s_5.setAttribute("style","color:black;")
+    let selected = 2;
+    let b = document.getElementById("value");
+    b.innerHTML = selected;
+}
+function three(){
+    let s_1 = document.getElementById("1");
+    let s_2 = document.getElementById("2");
+    let s_3 = document.getElementById("3");
+    let s_4 = document.getElementById("4");
+    let s_5 = document.getElementById("5");
+    s_1.setAttribute("style","color:#00cc66;")
+    s_2.setAttribute("style","color:#00cc66;")
+    s_3.setAttribute("style","color:#00cc66;")
+    s_4.setAttribute("style","color:black;")
+    s_5.setAttribute("style","color:black;")
+    let selected = 3;
+    let b = document.getElementById("value");
+    b.innerHTML = selected;
+}
+function four(){
+    let s_1 = document.getElementById("1");
+    let s_2 = document.getElementById("2");
+    let s_3 = document.getElementById("3");
+    let s_4 = document.getElementById("4");
+    let s_5 = document.getElementById("5");
+    s_1.setAttribute("style","color:#00cc66;")
+    s_2.setAttribute("style","color:#00cc66;")
+    s_3.setAttribute("style","color:#00cc66;")
+    s_4.setAttribute("style","color:#00cc66;")
+    s_5.setAttribute("style","color:black;")
+    let selected = 4;
+    let b = document.getElementById("value");
+    b.innerHTML = selected;
+}
+function five(){
+    let s_1 = document.getElementById("1");
+    let s_2 = document.getElementById("2");
+    let s_3 = document.getElementById("3");
+    let s_4 = document.getElementById("4");
+    let s_5 = document.getElementById("5");
+    s_1.setAttribute("style","color:#00cc66;")
+    s_2.setAttribute("style","color:#00cc66;")
+    s_3.setAttribute("style","color:#00cc66;")
+    s_4.setAttribute("style","color:#00cc66;")
+    s_5.setAttribute("style","color:#00cc66;")
+    let selected = 5;
+    let b = document.getElementById("value");
+    b.innerHTML = selected;
+}
